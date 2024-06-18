d084f629e390261cebece796a0e0253980b1a01b
DancollinsG <bufferdrake8052gmail.com>
2024-06-18 11:55:55 +0300

    new

---
+++ functions.js
@@ -0,0 +1,81 @@
+function division(){
+    let num1=10
+    let num2=5
+    let num3= num1/num2
+    console.log(num3)
+}
+division()
+
+function area(){
+    let see_area = document.getElementById("area")
+    let base= 24
+    let height= 12
+    let area=(base*height)/2
+    see_area.innerHTML = area
+}
+
+
+function volume(){
+    let see_volume = document.getElementById("volume")
+    let height= 24
+    let diameter= 14
+    let volume=(height*diameter)*3.142
+    see_volume.innerHTML = volume
+}
+
+function BMI(){
+    let see_BMI = document.getElementById("BMI")
+    let weight= 70
+    let height= 1.75
+    let BMI=weight/(height*height)
+    see_BMI.innerHTML = BMI.toFixed(2)
+}
+
+let num1=10
+let num2=5
+let num3= num1/num2
+console.log(num3)
+}
+division()
+
+function area(){
+let see_area = document.getElementById("area")
+let base= 24
+let height= 12
+let area=(base*height)/2
+see_area.innerHTML = area
+}
+
+
+function volume(){
+let see_volume = document.getElementById("volume")
+let height= 24
+let diameter= 14
+let volume=(height*diameter)*3.142
+see_volume.innerHTML = volume
+}
+
+function BMI(){
+let see_BMI = document.getElementById("BMI")
+let weight= 70
+let height= 1.75
+let BMI=weight/(height*height)
+see_BMI.innerHTML = BMI.toFixed(2)
+}
+
+function Simple_interest(){
+let see_simple_interest = document.getElementById("simple_interest")
+let principal= 10000
+let rate= 5
+let time= 2
+let simple_interest=(principal*rate*time)/100
+see_simple_interest.innerHTML = simple_interest
+}
+function Simple_interest(){
+    let see_simple_interest = document.getElementById("simple_interest")
+    let principal= 10000
+    let rate= 5
+    let time= 2
+    let simple_interest= (principal*rate*time)/100
+    see_simple_interest.innerHTML = simple_interest
+}
---
+++ js main.js
@@ -0,0 +1,15 @@
+const myclass ="lab 2";
+let myschool ="modcom";
+student ="Mike;
+
+myclass="lab3"
+myschool="uon"
+student="Mike"
+
+console.log(student)
+console.log(typeof(student))
+console.log(myschool)
+console.log(typeof(myschool))
+console.log(student)
+console.log(student)
+console.log(typeof(student))
